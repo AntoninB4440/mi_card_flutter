afdca2404746bd1ffbb74356cb4a85a87b82762d
AntoninB4440 <barceloantonin@gmail.com>
2021-05-15 18:41:45 +0200

    First commit + adding CircleAvatar + name

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,33 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+          child: Column(
+            children: [
+              CircleAvatar(
+                  radius: 50,
+                  backgroundImage: AssetImage('images/antonin.jpg')),
+              Text(
+                'Antonin Barcelo',
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 22.0,
+                    fontWeight: FontWeight.bold),
+              ),
+            ],
+          ),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
